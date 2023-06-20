2acafb6872f7c69cc7d4e091e2056776b519d612
Mariangelica Rodriguez <gromarant@gmail.com>
2023-06-20 22:06:49 +0200

    Add home page to regular users

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
+let pug = require("pug")
 const app = express();
 const PORT = 3000;
 
 const helmet = require("helmet");
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'))
+
 app.use(helmet());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Aquí irá el login");
-});
+app.get("/", (req, res) => res.render('home'));
+app.get("/favorites", (req, res) => res.render('favorites'));
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en el puerto ${PORT}`);
