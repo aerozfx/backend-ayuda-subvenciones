60ba3f856c89405229c61230adf0edc1985a8f9c
Mariangelica Rodriguez <gromarant@gmail.com>
2023-06-23 15:34:37 +0200

    Add users list View

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -33,15 +33,60 @@ const homePageController = async(req, res) => {
   }
 };
 
-const favoritesPageController = (req, res) => res.render('favorites', { "page_title": "favoritos", scrapingData });
+// const favoritesPageController = (req, res) => res.render('favorites', { "page_title": "favoritos", scrapingData });
 
 const profilePageController = (req, res) => res.render('profile', {"page_title": "perfil"});
 
 //Por hacer post-logout
 
+const users = [
+  {
+    "user_id": 1,
+    "name": "Mariangelica",
+    "surname": "Rodriguez",
+    "email": "marian@gmail.com",
+    "password": "hcipqy1r123",
+    "role": "admin"
+  },
+  {
+    "user_id": 2,
+    "name": "Geronimo",
+    "surname": "Lopez",
+    "email": "gero@gmail.com",
+    "password": "hcikxpow123",
+    "role": "user"
+  },
+  {
+    "user_id": 3,
+    "name": "Mariangelica",
+    "surname": "Rodriguez",
+    "email": "marian@gmail.com",
+    "password": "hcipqy1r123",
+    "role": "user"
+  },
+  {
+    "user_id": 4,
+    "name": "Miguel",
+    "surname": "Varon",
+    "email": "migue@gmail.com",
+    "password": "kowejy1r123",
+    "role": "user"
+  },
+]
+const usersListController = (req, res) => {
+  try {
+    if (users) {
+      res.render('users', { "page_title": "users", users})
+    }
+  }
+  catch(error) {
+    res.status(400).json({ msj: `ERROR ${error}`});
+  }
+};
 
 module.exports = {
   homePageController,
-  favoritesPageController,
-  profilePageController
+  // favoritesPageController,
+  profilePageController,
+  usersListController
 }
