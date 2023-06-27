fa905c8e6ea2ff53ea88b09f1a73d58184b347fb
Mariangelica Rodriguez <gromarant@gmail.com>
2023-06-27 17:58:51 +0200

    Update view when a favorite is delete

--- public/script.js
+++ public/script.js
@@ -67,5 +67,9 @@ async function deleteFavorite(grantId) {
   }
 };
 
-selectElements('.addFavorites').forEach(grantCard => grantCard.addEventListener('click', () => postFavorite(grantCard.getAttribute('id'))));
-selectElements('.deleteFavorite').forEach(grantCard => grantCard.addEventListener('click', () => deleteFavorite((grantCard.getAttribute('id')))));
+selectElements('.addFavorites').forEach(addBtn => addBtn.addEventListener('click', () => postFavorite(addBtn.getAttribute('id'))));
+selectElements('.deleteFavorite').forEach(deleteBtn => deleteBtn.addEventListener('click', () => {
+  const suffixId = deleteBtn.getAttribute('id');
+  selectElement(`#favorite-${suffixId}`).classList.add("hidden");
+  deleteFavorite((deleteBtn.getAttribute('id')));
+}));
