796d67ec9605e28a625db26c9c898f32e67649a6
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-06-28 10:45:45 +0200

    add/docs are done!

--- models/favorites.js
+++ models/favorites.js
@@ -1,6 +1,20 @@
+/**
+ * @exports routes 
+ * @namespace favorites_model 
+ */
+
 const pool = require("../utils/db-sql-favorites");
 const db_favorites_queries = require("./queries/favorites.queries");
 
+
+/** 
+* @memberof favorites_model 
+* @method addFavorite 
+* @async 
+* @param {Object} data id de subvencion favorita y id de usuario
+* @return {Object} Objeto de la subvencion favorita
+* @throws {error} 
+*/
 const addFavorite = async (data) => {
   let { favorite_id, user_id } = data;
   let client, result;
@@ -19,6 +33,15 @@ const addFavorite = async (data) => {
   return result;
 };
 
+
+/** 
+* @memberof favorites_model 
+* @method removeFavorite 
+* @async 
+* @param {Object} data id de subvencion favorita y id de usuario
+* @return {Object} devuelve el objeto actualizado despues de eliminar la subvencion favorita elegida 
+* @throws {error} 
+*/
 const removeFavorite = async ({ favorite_id, user_id }) => {
   let client, result;
   try {
@@ -36,6 +59,13 @@ const removeFavorite = async ({ favorite_id, user_id }) => {
   return result;
 };
 
+/** 
+* @memberof favorites_model 
+* @method removeFavorite 
+* @async 
+* @return {Object} devuelve un objeto con todas las subvenciones
+* @throws {error} 
+*/
 const getFavorites = async () => {
   let client, result;
   try {
@@ -49,6 +79,15 @@ const getFavorites = async () => {
   }
   return result;
 };
+
+/** 
+* @memberof favorites_model 
+* @method getFavoritesByUserId 
+* @async 
+* @param {Object} id number
+* @return {Object} retorna un objeto con el usuario que tiene el id que hemos pasado por parametro
+* @throws {error} 
+*/
 const getFavoritesByUserId = async (id) => {
   let client, result;
   try {
--- models/users.js
+++ models/users.js
@@ -1,5 +1,19 @@
+/**
+ * @exports routes 
+ * @namespace users_model 
+ */
+
 const db_users_queries = require("./queries/users.queries.js");
 const pool = require("../utils/db-sql-users.js");
+
+/** 
+* @memberof users_model
+* @method getUserByEmail 
+* @async 
+* @param {string} email email de usuario
+* @return {Object} Objeto del usuario que coincide con el email dado por paramtros
+* @throws {error} 
+*/
 
 const getUserByEmail = async (email) => {
   let client, result;
@@ -15,6 +29,14 @@ const getUserByEmail = async (email) => {
   }
   return result;
 };
+
+/** 
+* @memberof users_model
+* @method getUsers 
+* @async 
+* @return {Object} retorna un objeto con todos los usuarios
+* @throws {error} 
+*/
 const getUsers = async () => {
   let client, result;
   try {
@@ -29,6 +51,15 @@ const getUsers = async () => {
   }
   return result;
 };
+
+/** 
+* @memberof users_model
+* @method createUser 
+* @async 
+* @param {Object} data Objeto con los datos del usuario
+* @return {Object} objeto del usuario creado
+* @throws {error} 
+*/
 const createUser = async (data) => {
   let { email, name, surname, password, role } = data;
   let client, result;
@@ -51,6 +82,14 @@ const createUser = async (data) => {
   return result;
 };
 
+/** 
+* @memberof users_model
+* @method updateUser 
+* @async 
+* @param {Object} data id de subvencion favorita y id de usuario
+* @return {Object} Objeto del usuario modificado
+* @throws {error} 
+*/
 const updateUser = async (data) => {
   console.log(data);
   let { newName, newSurname, newEmail, newPassword, newRole, email } = data;
@@ -75,6 +114,14 @@ const updateUser = async (data) => {
   return result;
 };
 
+/** 
+* @memberof users_model
+* @method deleteUser 
+* @async 
+* @param {string} email email de usuario 
+* @return {Object} devuelve el objeto actualizado despues de eliminar la subvencion favorita elegida 
+* @throws {error} 
+*/
 const deleteUser = async (email) => {
   let client, result;
   try {
@@ -89,6 +136,7 @@ const deleteUser = async (email) => {
   }
   return result;
 };
+
 
 const users = {
   createUser,
